bc7ff6e50bd35f295a187f6768ff2dc3492442c9
Victor de Souza <victor88dsds88@gmail.com>
2025-12-17 23:10:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h2>Conteúdo da página</h2>
+    </>
+  );
+}
+
+    
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ background: "#111", color: "#fff", padding: "20px" }}>
+      <h1>Meu Header</h1>
+    </header>
+  );
+}
+
+export default Header;
